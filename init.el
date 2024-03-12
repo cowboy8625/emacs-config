aeb384e37a988f9a252130fcd10881abac3283ee
cowboy8625 <cowboy8625@yahoo.com>
2024-03-11 20:19:19 -0500

    magit and rebind neotree with evil bindings

--- init.el
+++ init.el
@@ -10,7 +10,6 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("e5f7bfb2f2186f8225337c32f706f34b88ffa86f3ffa1c7e4723e4f82eb5389f" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" "944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "636b135e4b7c86ac41375da39ade929e2bd6439de8901f53f88fde7dd5ac3561" "1f669e8abe4dc2855268c9a607b5e350e2811b3c5afd09af5939ff0c01a89c5a" default))
- '(package-selected-packages '(eglot))
  '(warning-suppress-log-types '((emacs))))
 
 (custom-set-faces
